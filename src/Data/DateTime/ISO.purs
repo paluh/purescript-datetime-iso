48aef1c1b19aa753ba118a0547e973288c4744bc
Jamie Bertram <jamie.bertram@tweag.io>
2021-11-01 13:10:47 -0400

    Configure purs-tidy

--- src/Data/DateTime/ISO.purs
+++ src/Data/DateTime/ISO.purs
@@ -33,25 +33,25 @@ unwrapISO :: ISO -> DateTime
 unwrapISO = unwrap
 
 instance showISO :: Show ISO where
-    show (ISO (DateTime date time)) = foldl append ""
-        [ showInt $ DT.year date
-        , "-"
-        , padl 2 '0' $ showInt $ DT.month date
-        , "-"
-        , padl 2 '0' $ showInt $ DT.day date
-        , "T"
-        , padl 2 '0' $ showInt $ DT.hour time
-        , ":"
-        , padl 2 '0' $ showInt $ DT.minute time
-        , ":"
-        , padl 2 '0' $ showInt $ DT.second time
-        , "."
-        , removeTrailingZeros $ padMilli $ DT.millisecond time
-        , "Z"
-        ]
-        where
-            showInt :: forall a. BoundedEnum a => a -> String
-            showInt = show <<< fromEnum
+  show (ISO (DateTime date time)) = foldl append ""
+    [ showInt $ DT.year date
+    , "-"
+    , padl 2 '0' $ showInt $ DT.month date
+    , "-"
+    , padl 2 '0' $ showInt $ DT.day date
+    , "T"
+    , padl 2 '0' $ showInt $ DT.hour time
+    , ":"
+    , padl 2 '0' $ showInt $ DT.minute time
+    , ":"
+    , padl 2 '0' $ showInt $ DT.second time
+    , "."
+    , removeTrailingZeros $ padMilli $ DT.millisecond time
+    , "Z"
+    ]
+    where
+    showInt :: forall a. BoundedEnum a => a -> String
+    showInt = show <<< fromEnum
 
 -- | Pad an integer from a millisecond value with enough zeros so it is three
 -- digits.
@@ -62,65 +62,70 @@ padMilli = padl 3 '0' <<< show <<< fromEnum
 removeTrailingZeros :: String -> String
 removeTrailingZeros "000" = "0"
 removeTrailingZeros s =
-  fromCodePointArray <<<
-  Array.reverse <<<
-  Array.dropWhile (_ == codePointFromChar '0') <<<
-  Array.reverse $
-  toCodePointArray s
+  fromCodePointArray
+    <<< Array.reverse
+    <<< Array.dropWhile (_ == codePointFromChar '0')
+    <<<
+      Array.reverse $
+    toCodePointArray s
 
 instance decodeJsonISO :: DecodeJson ISO where
-    decodeJson = decodeJson
-             >=> flip P.runParser (parseISO :: P.Parser String ISO)
-             >>> lmap P.parseErrorMessage
+  decodeJson = decodeJson
+    >=> flip P.runParser (parseISO :: P.Parser String ISO)
+      >>> lmap P.parseErrorMessage
 
 instance encodeJsonISO :: EncodeJson ISO where
-    encodeJson = show >>> encodeJson
+  encodeJson = show >>> encodeJson
 
 --------------------------------------------------------------------------------
 
 parseISO :: forall s m. Monad m => PS.StringLike s => P.ParserT s m ISO
 parseISO = do
-    date <- parseISODate
-    _ <- PS.char 'T'
-    time <- parseISOTime
-    pure $ wrap $ DateTime date time
+  date <- parseISODate
+  _ <- PS.char 'T'
+  time <- parseISOTime
+  pure $ wrap $ DateTime date time
 
 parseISODate :: forall s m. Monad m => PS.StringLike s => P.ParserT s m Date
 parseISODate = do
-    year  <- parseDigits 4 <#> toEnum >>= maybeFail "bad year"
-    _     <- dash
-    month <- parseDigits 2 <#> toEnum >>= maybeFail "bad month"
-    _     <- dash
-    day   <- parseDigits 2 <#> toEnum >>= maybeFail "bad day"
-    DT.exactDate year month day # maybeFail "bad date"
+  year <- parseDigits 4 <#> toEnum >>= maybeFail "bad year"
+  _ <- dash
+  month <- parseDigits 2 <#> toEnum >>= maybeFail "bad month"
+  _ <- dash
+  day <- parseDigits 2 <#> toEnum >>= maybeFail "bad day"
+  DT.exactDate year month day # maybeFail "bad date"
 
-    where dash  = PC.optional $ PC.try $ PS.char '-'
+  where
+  dash = PC.optional $ PC.try $ PS.char '-'
 
 parseISOTime :: forall s m. Monad m => PS.StringLike s => P.ParserT s m Time
 parseISOTime = do
-    hh <- parseDigits 2 <#> toEnum >>= maybeFail "bad hour"
-    _  <- colon
-    mm <- parseDigits 2 <#> toEnum >>= maybeFail "bad minute"
-    _  <- colon
-    ss <- parseDigits 2 <#> toEnum >>= maybeFail "bad second"
-    -- NOTE: milliseconds may not be present
-    ms <- PC.option bottom $
-                PC.try (PS.char '.') *> Array.some parseDigit <#> doMilli
-    pure $ DT.Time hh mm ss ms
-    where
-        colon = PC.optional $ PC.try $ PS.char ':'
+  hh <- parseDigits 2 <#> toEnum >>= maybeFail "bad hour"
+  _ <- colon
+  mm <- parseDigits 2 <#> toEnum >>= maybeFail "bad minute"
+  _ <- colon
+  ss <- parseDigits 2 <#> toEnum >>= maybeFail "bad second"
+  -- NOTE: milliseconds may not be present
+  ms <- PC.option bottom $
+    PC.try (PS.char '.') *> Array.some parseDigit <#> doMilli
+  pure $ DT.Time hh mm ss ms
+  where
+  colon = PC.optional $ PC.try $ PS.char ':'
 
 -- | Parse an array of three integers into a `Millisecond` value.
 doMilli :: Array Int -> Millisecond
 doMilli ns =
-  let padded = Array.take 3 $ ns <> Array.replicate (3 - length ns) 0
-  in fromMaybe top $ toEnum $ foldDigits padded
+  let
+    padded = Array.take 3 $ ns <> Array.replicate (3 - length ns) 0
+  in
+    fromMaybe top $ toEnum $ foldDigits padded
 
 parseDigits :: forall s m. Monad m => PS.StringLike s => Int -> P.ParserT s m Int
 parseDigits = map foldDigits <<< sequence <<< flip Array.replicate parseDigit
 
 parseDigit :: forall s m. Monad m => PS.StringLike s => P.ParserT s m Int
-parseDigit = PC.choice
+parseDigit =
+  PC.choice
     [ PS.char '0' *> pure 0
     , PS.char '1' *> pure 1
     , PS.char '2' *> pure 2
@@ -143,9 +148,9 @@ maybeFail str = maybe (P.fail str) pure
 
 padl :: Int -> Char -> String -> String
 padl n chr str = String.fromCharArray $
-    padl' (n - String.length str) chr (String.toCharArray str)
+  padl' (n - String.length str) chr (String.toCharArray str)
 
 padl' :: Int -> Char -> Array Char -> Array Char
 padl' n chr chrs
-    | n <= 0 = chrs
-    | otherwise = padl' (n - 1) chr (chr `Array.cons` chrs)
+  | n <= 0 = chrs
+  | otherwise = padl' (n - 1) chr (chr `Array.cons` chrs)
--- test/Main.purs
+++ test/Main.purs
@@ -18,169 +18,169 @@ import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [consoleReporter] do
-    describe "purescript-datetime-iso" do
+main = launchAff_ $ runSpec [ consoleReporter ] do
+  describe "purescript-datetime-iso" do
 
-        describe "decoding" do
+    describe "decoding" do
 
-            it "decodes standard js ISO strings (ala Date.prototype.toISOString)" do
-                case decodeISO "2018-01-09T13:16:43.772Z" of
-                    Left err ->
-                        fail $ "decoding failed: " <> err
-                    Right iso ->
-                        unwrapISO iso `shouldEqual`
-                            mkDateTime 2018 DT.January 9 13 16 43 772
+      it "decodes standard js ISO strings (ala Date.prototype.toISOString)" do
+        case decodeISO "2018-01-09T13:16:43.772Z" of
+          Left err ->
+            fail $ "decoding failed: " <> err
+          Right iso ->
+            unwrapISO iso `shouldEqual`
+              mkDateTime 2018 DT.January 9 13 16 43 772
 
-            describe "optional characters" do
+      describe "optional characters" do
 
-                it "doesn't need hyphens in the date" do
-                    case decodeISO "20180109T13:16:43.772Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            unwrapISO iso `shouldEqual`
-                                mkDateTime 2018 DT.January 9 13 16 43 772
+        it "doesn't need hyphens in the date" do
+          case decodeISO "20180109T13:16:43.772Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              unwrapISO iso `shouldEqual`
+                mkDateTime 2018 DT.January 9 13 16 43 772
 
-                it "doesn't need colons in the time" do
-                    case decodeISO "20180109T131643.772Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            unwrapISO iso `shouldEqual`
-                                mkDateTime 2018 DT.January 9 13 16 43 772
+        it "doesn't need colons in the time" do
+          case decodeISO "20180109T131643.772Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              unwrapISO iso `shouldEqual`
+                mkDateTime 2018 DT.January 9 13 16 43 772
 
-            describe "milliseconds" do
+      describe "milliseconds" do
 
-                it "handles zero milliseconds" do
-                    case decodeISO "2018-01-09T13:16:43.0Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            unwrapISO iso `shouldEqual`
-                                mkDateTime 2018 DT.January 9 13 16 43 0
+        it "handles zero milliseconds" do
+          case decodeISO "2018-01-09T13:16:43.0Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              unwrapISO iso `shouldEqual`
+                mkDateTime 2018 DT.January 9 13 16 43 0
 
-                it "handles empty milliseconds" do
-                    case decodeISO "2018-01-09T13:16:43Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            unwrapISO iso `shouldEqual`
-                                mkDateTime 2018 DT.January 9 13 16 43 0
+        it "handles empty milliseconds" do
+          case decodeISO "2018-01-09T13:16:43Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              unwrapISO iso `shouldEqual`
+                mkDateTime 2018 DT.January 9 13 16 43 0
 
-                it "handles milliseconds 0-999" do
-                    case decodeISO "2018-01-09T13:16:43.999Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            unwrapISO iso `shouldEqual`
-                                mkDateTime 2018 DT.January 9 13 16 43 999
+        it "handles milliseconds 0-999" do
+          case decodeISO "2018-01-09T13:16:43.999Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              unwrapISO iso `shouldEqual`
+                mkDateTime 2018 DT.January 9 13 16 43 999
 
-                it "handles more than 3 digits second fraction" do
-                    case decodeISO "2018-01-09T13:16:43.1234Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            show iso `shouldEqual`
-                                "2018-01-09T13:16:43.123Z"
+        it "handles more than 3 digits second fraction" do
+          case decodeISO "2018-01-09T13:16:43.1234Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              show iso `shouldEqual`
+                "2018-01-09T13:16:43.123Z"
 
-                it "handles milliseconds with one leading zero" do
-                    case decodeISO "2018-01-09T03:16:43.034Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            unwrapISO  iso `shouldEqual`
-                                mkDateTime 2018 DT.January 9 3 16 43 34
+        it "handles milliseconds with one leading zero" do
+          case decodeISO "2018-01-09T03:16:43.034Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              unwrapISO iso `shouldEqual`
+                mkDateTime 2018 DT.January 9 3 16 43 34
 
-                it "handles milliseconds with two leading zeros" do
-                    case decodeISO "2018-01-09T13:06:33.002Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            show iso `shouldEqual`
-                                "2018-01-09T13:06:33.002Z"
+        it "handles milliseconds with two leading zeros" do
+          case decodeISO "2018-01-09T13:06:33.002Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              show iso `shouldEqual`
+                "2018-01-09T13:06:33.002Z"
 
-                it "handles two digit milliseconds with leading zero" do
-                    case decodeISO "2018-01-09T13:26:03.07Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            show iso `shouldEqual`
-                                "2018-01-09T13:26:03.07Z"
+        it "handles two digit milliseconds with leading zero" do
+          case decodeISO "2018-01-09T13:26:03.07Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              show iso `shouldEqual`
+                "2018-01-09T13:26:03.07Z"
 
-                it "handles two digit milliseconds with leading and trailing zero" do
-                    case decodeISO "2018-01-09T13:06:03.070Z" of
-                        Left err ->
-                            fail $ "decoding failed: " <> err
-                        Right iso ->
-                            show iso `shouldEqual`
-                                "2018-01-09T13:06:03.07Z"
+        it "handles two digit milliseconds with leading and trailing zero" do
+          case decodeISO "2018-01-09T13:06:03.070Z" of
+            Left err ->
+              fail $ "decoding failed: " <> err
+            Right iso ->
+              show iso `shouldEqual`
+                "2018-01-09T13:06:03.07Z"
 
-            describe "malformed input" do  -- malformed as far as we're concerned...
+      describe "malformed input" do -- malformed as far as we're concerned...
 
-                it "fails if not YYYY MM DD" do
-                    case decodeISO "2018-1-9T13:16:43.1Z" of
-                        Left err -> do
-                            pure unit
-                        Right _ ->
-                            fail "shouldn't have parsed"
+        it "fails if not YYYY MM DD" do
+          case decodeISO "2018-1-9T13:16:43.1Z" of
+            Left err -> do
+              pure unit
+            Right _ ->
+              fail "shouldn't have parsed"
 
-                it "requires a terminating 'Z' (UTC)" do
-                    case decodeISO "2018-1-9T13:16:43.0" of
-                        Left err -> do
-                            pure unit
-                        Right _ ->
-                            fail "shouldn't have parsed"
+        it "requires a terminating 'Z' (UTC)" do
+          case decodeISO "2018-1-9T13:16:43.0" of
+            Left err -> do
+              pure unit
+            Right _ ->
+              fail "shouldn't have parsed"
 
-        describe "printing" do
+    describe "printing" do
 
-            it "prints like an ISO string" do
-                let dt = mkDateTime 2018 DT.January 9 13 16 43 772
-                show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.772Z"
+      it "prints like an ISO string" do
+        let dt = mkDateTime 2018 DT.January 9 13 16 43 772
+        show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.772Z"
 
-            it "explicitly prints zero milliseconds" do
-                let dt = mkDateTime 2018 DT.January 9 13 16 43 0
-                show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.0Z"
+      it "explicitly prints zero milliseconds" do
+        let dt = mkDateTime 2018 DT.January 9 13 16 43 0
+        show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.0Z"
 
-            it "prints milliseconds with two leading zeros" do
-                let dt = mkDateTime 2018 DT.January 9 13 16 43 3
-                show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.003Z"
+      it "prints milliseconds with two leading zeros" do
+        let dt = mkDateTime 2018 DT.January 9 13 16 43 3
+        show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.003Z"
 
-            it "prints milliseconds with one leading zero" do
-                let dt = mkDateTime 2018 DT.January 9 13 16 43 40
-                show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.04Z"
+      it "prints milliseconds with one leading zero" do
+        let dt = mkDateTime 2018 DT.January 9 13 16 43 40
+        show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.04Z"
 
-            it "removes unneeded zeros from end of milliseconds" do
-                let dt = mkDateTime 2018 DT.January 9 13 16 43 840
-                show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.84Z"
+      it "removes unneeded zeros from end of milliseconds" do
+        let dt = mkDateTime 2018 DT.January 9 13 16 43 840
+        show (ISO dt) `shouldEqual` "2018-01-09T13:16:43.84Z"
 
 decodeISO :: String -> Either String ISO
 decodeISO = fromString >>> decodeJson
 
 -- Helper function for constructing DateTimes.
 mkDateTime
-    :: Int
-    -> DT.Month
-    -> Int
-    -> Int
-    -> Int
-    -> Int
-    -> Int
-    -> DT.DateTime
+  :: Int
+  -> DT.Month
+  -> Int
+  -> Int
+  -> Int
+  -> Int
+  -> Int
+  -> DT.DateTime
 mkDateTime year month day hh mm ss ms =
-    let
-        date =
-            DT.canonicalDate
-                (toEnum' year)
-                month
-                (toEnum' day)
-        time =
-            DT.Time
-                (toEnum' hh)
-                (toEnum' mm)
-                (toEnum' ss)
-                (toEnum' ms)
-    in
-        DT.DateTime date time
-    where
-        toEnum' :: forall a. BoundedEnum a => Int -> a
-        toEnum' = toEnum >>> unsafePartial fromJust
+  let
+    date =
+      DT.canonicalDate
+        (toEnum' year)
+        month
+        (toEnum' day)
+    time =
+      DT.Time
+        (toEnum' hh)
+        (toEnum' mm)
+        (toEnum' ss)
+        (toEnum' ms)
+  in
+    DT.DateTime date time
+  where
+  toEnum' :: forall a. BoundedEnum a => Int -> a
+  toEnum' = toEnum >>> unsafePartial fromJust
